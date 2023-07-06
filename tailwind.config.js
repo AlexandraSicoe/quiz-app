44ef7cf8256cafa566271b38ba677e2c384ecd55
Lex <sicoealexandra97@gmail.com>
2023-07-06 16:00:34 +0300

    cleaned app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    colors: {
-      blue: "#1fb6ff",
-      lightblue: "#ADD8E6",
-      CornflowerBlue: "#6495ED",
-      DodgerBlue: "#1E90FF",
-      DeepSkyBlue: "#00BFFF",
-      MintCream: "#F5FFFA",
-    },
-    theme: {
-      container: {
-        center: true,
-      },
-    },
-    extend: {},
-  },
+
   plugins: [],
 };
